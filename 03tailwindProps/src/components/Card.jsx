6dd406ac1d296549f31c04bf5f908f180769d9c0
indresh-007 <140510462+indresh-007@users.noreply.github.com>
2023-10-26 17:49:44 +0530

    interview

--- 03tailwindProps/src/components/Card.jsx
+++ 03tailwindProps/src/components/Card.jsx
@@ -9,14 +9,19 @@ function Card({username, btnText}) {
         alt="AirMax Pro"
         className="z-0 h-full w-full rounded-md object-cover"
       />
-       <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+      <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+
       <div className="absolute bottom-4 left-4 text-left">
+
        <h1 className="text-lg font-semibold text-white">{username}</h1>
+
        <p className="mt-2 text-sm text-gray-300">
        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
        debitis?
        </p>
+       
        <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+
        {btnText}→
        </button>
       </div>
